a5e81c4d035d1f2ec6062b9ae14f1c4ca4573d70
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-01-18 08:07:46 -0800

    seed silks

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_221928) do
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.text "name", null: false
-    t.text "body"
+    t.string "name", null: false
+    t.string "body"
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_221928) do
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.text "name", null: false
+    t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
@@ -35,20 +35,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_221928) do
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.text "key", null: false
-    t.text "filename", null: false
-    t.text "content_type"
-    t.text "metadata"
-    t.text "service_name", null: false
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.string "metadata"
+    t.string "service_name", null: false
     t.bigint "byte_size", null: false
-    t.text "checksum"
+    t.string "checksum"
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
-    t.text "variation_digest", null: false
+    t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
@@ -63,53 +63,53 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_221928) do
   end
 
   create_table "dmcs", force: :cascade do |t|
-    t.text "number"
-    t.text "color"
-    t.text "rgb_value"
-    t.text "image"
-    t.text "stash"
+    t.string "number"
+    t.string "color"
+    t.string "rgb_value"
+    t.string "image"
+    t.string "stash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "metallics", force: :cascade do |t|
-    t.text "brand"
-    t.text "thread_type"
-    t.text "number"
-    t.text "color"
-    t.text "pattern"
-    t.text "image"
-    t.text "stash"
+    t.string "brand"
+    t.string "thread_type"
+    t.string "number"
+    t.string "color"
+    t.string "pattern"
+    t.string "image"
+    t.string "stash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "patterns", force: :cascade do |t|
-    t.text "brand"
-    t.text "pattern_name"
-    t.text "number"
-    t.text "size"
+    t.string "brand"
+    t.string "pattern_name"
+    t.string "number"
+    t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
   end
 
   create_table "silks", force: :cascade do |t|
-    t.text "brand"
-    t.text "thread_type"
-    t.text "number"
-    t.text "color"
-    t.text "pattern"
-    t.text "image"
-    t.text "stash"
+    t.string "brand"
+    t.string "thread_type"
+    t.string "number"
+    t.string "color"
+    t.string "pattern"
+    t.string "image"
+    t.string "stash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "email", default: "", null: false
-    t.text "encrypted_password", default: "", null: false
-    t.text "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
