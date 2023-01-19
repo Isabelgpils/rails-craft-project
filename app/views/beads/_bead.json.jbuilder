fadf0516d970665a04cbce656ef575d892ac0a83
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-01-18 23:03:18 -0800

    beads seeding, views sorting, style fixes

--- app/views/beads/_bead.json.jbuilder
+++ app/views/beads/_bead.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! bead, :id, :number, :color, :pattern, :image, :stash, :created_at, :updated_at
+json.extract! bead, :id, :number, :color, pattern:[], :image, :stash, :created_at, :updated_at
 json.url bead_url(bead, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,17 @@
 
 User.create(email: "test@email.com", password: "password", password_confirmation: "password")
 
-text_from_json = File.read("#{Rails.root.to_s}/db/seeds/dinky_dyes.json")
-JSON.parse(text_from_json).each do |t|
-  Silk.create! ([{brand: t['brand'], thread_type: t["thread_type"], number: t['number'], color: t['color'] }] )
-end
+# text_from_json = File.read("#{Rails.root.to_s}/db/seeds/dinky_dyes.json")
+# JSON.parse(text_from_json).each do |t|
+#   Silk.create! ([{brand: t['brand'], thread_type: t["thread_type"], number: t['number'], color: t['color'] }] )
+# end
 
-text_from_json = File.read("#{Rails.root.to_s}/db/seeds/metallics.json")
+# text_from_json = File.read("#{Rails.root.to_s}/db/seeds/metallics.json")
+# JSON.parse(text_from_json).each do |t|
+#   Metallic.create! ([{brand: t['brand'], thread_type: t['thread_type'], number: t['number'], color: t['color'], pattern: t['pattern'], stash: t['stash'] }] )
+# end
+Bead.destroy_all
+text_from_json = File.read("#{Rails.root.to_s}/db/seeds/beads.json")
 JSON.parse(text_from_json).each do |t|
-  Metallic.create! ([{brand: t['brand'], thread_type: t['thread_type'], number: t['number'], color: t['color'], pattern: t['pattern'], stash: t['stash'] }] )
-end
-
-text_from_json = File.read("#{Rails.root.to_s}/db/seeds/patterns.json")
-JSON.parse(text_from_json).each do |t|
-  Pattern.create! ([{brand: t['brand'], pattern_name: t['pattern_name'], number: t['number'], size: t['size'] }] )
+  Bead.create! ([{number: t['number'], pattern: t['pattern']}] )
 end
