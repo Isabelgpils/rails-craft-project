ffe0c966ac38f4ee1769855029bdce81904eef04
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-01-21 10:07:05 -0800

    add missing class for responsive navigation

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 // = link_tree ../images
 // = link_tree ../builds
+<<<<<<< Updated upstream
+=======
+//= link_tree ../../javascript .js
+
+>>>>>>> Stashed changes
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
+<<<<<<< Updated upstream
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
@@ -11,3 +12,12 @@ document.addEventListener("turbo:load", () => {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })
+=======
+import * as bootstrap from "bootstrap"
+// import "@hotwired/turbo-rails"
+import "./builds/application.css"
+import "./controllers"
+import "trix"
+import "@rails/actiontext"
+import * as bootstrap from "bootstrap"
+>>>>>>> Stashed changes
