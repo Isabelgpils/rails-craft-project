a2a6cfc29fb99be1bef6b371b460788318366871
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-01-21 10:11:40 -0800

    fix merge conflict

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,4 @@
 // = link_tree ../images
 // = link_tree ../builds
-<<<<<<< Updated upstream
-=======
 //= link_tree ../../javascript .js
 
->>>>>>> Stashed changes
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,4 @@
 // Entry point for the build script in your package.json
-<<<<<<< Updated upstream
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-import "trix"
-import "@rails/actiontext"
-
-document.addEventListener("turbo:load", () => {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})
-=======
 import * as bootstrap from "bootstrap"
 // import "@hotwired/turbo-rails"
 import "./builds/application.css"
@@ -20,4 +6,3 @@ import "./controllers"
 import "trix"
 import "@rails/actiontext"
 import * as bootstrap from "bootstrap"
->>>>>>> Stashed changes
