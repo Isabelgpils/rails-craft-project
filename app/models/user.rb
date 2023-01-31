56a37f8c72ccdf210393f5e06b2682344a83e6c7
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-01-31 13:26:25 -0800

    set user admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: [ :user, :admin ]
+  after_initialize :set_default_role, :if => :new_record?
+  
+    def set_default_role
+      self.role ||= :user
+    end
+
   has_many :patterns
+
 end
