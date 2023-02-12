d941b58e03353e50363f4a3769b7083b83cef970
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-02-12 14:19:27 -0800

    changes to metallics model and fix to metallic form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_154022) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_213516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_154022) do
     t.string "stash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "metallic_type"
   end
 
   create_table "metallics_patterns", id: false, force: :cascade do |t|
