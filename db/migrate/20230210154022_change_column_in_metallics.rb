b35da424babd4c98daae8b93d1f24a61857fb72b
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-02-17 16:04:22 -0600

    remove default from metallics

--- db/migrate/20230210154022_change_column_in_metallics.rb
+++ db/migrate/20230210154022_change_column_in_metallics.rb
@@ -1,9 +1,9 @@
 class ChangeColumnInMetallics < ActiveRecord::Migration[7.0]
   def down
 
-    change_column :metallics, :pattern, array: true, default: []
+    change_column :metallics, :pattern, :string, array: true, default: []
   end
   def up
-    change_column :metallics, :pattern, :string, array: true, default: []
+    change_column :metallics, :pattern, :string, array: true
   end
 end
