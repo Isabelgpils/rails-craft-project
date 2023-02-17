450fbfd570815176a4797181894ddc1e44829b62
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-02-17 16:13:03 -0600

    deletes migrations

--- db/migrate/20230209122024_create_pattern_metallic_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreatePatternMetallicJoinTable < ActiveRecord::Migration[7.0]
-
-  # def change
-  #   create_join_table :patterns, :metallics do |t|
-  #     t.index :pattern_id
-  #     t.index :metallic_id
-  #   end
-  # end
-end
--- db/migrate/20230210154022_change_column_in_metallics.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnInMetallics < ActiveRecord::Migration[7.0]
-  def down
-
-    change_column :metallics, :pattern, :string, array: true, default: []
-  end
-  def up
-    change_column :metallics, :pattern, :string, array: true
-  end
-end
