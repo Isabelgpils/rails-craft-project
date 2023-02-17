d947c9460a23f05822da9b7643d0718f9a160958
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-02-17 15:13:17 -0600

    delete metallic associations to patterns

--- db/migrate/20230209120721_add_associations_to_patterns_metallics.rb
+++
@@ -1,8 +0,0 @@
-class AddAssociationsToPatternsMetallics < ActiveRecord::Migration[7.0]
-  def change
-    create_table :metallics_patterns, id: false do |t|
-      t.belongs_to :pattern
-      t.belongs_to :metallic
-    end
-  end
-end
