d61d478db80f5c677b6aa7407777fcd36ac1a070
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2023-02-17 15:55:52 -0600

    fix two migrations with errors

--- db/migrate/20230212213516_change_metallic_column_default.rb
+++ db/migrate/20230212213516_change_metallic_column_default.rb
@@ -2,16 +2,4 @@ class ChangeMetallicColumnDefault < ActiveRecord::Migration[7.0]
   def change
     add_column :metallics, :metallic_type, :string
   end
-
-  def up
-    change_table :metallics do |t|
-      t.change :patterns, :string, array: true
-    end
-  end
-
-  def down
-    change_table :metallics do |t|
-      t.change :patterns, :text, array: true, default: []
-    end
-  end
 end
